134d206951a294967e9e267478e76ea6bc84f556
YukeChan2002 <yukeshchandran002@gmail.com>
2024-06-28 18:25:15 +0530

    final code

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "./components/Card";
+
+
+
+function App() {
+  return (
+    <>
+      <div className="d-flex flex-column justify-content-center align-items-center CardCss">
+        <Card />
+      </div>
+    </>
+  );
+}
+
+export default App;
